2c15d6600bddfdb31faf1aaa6fbb5f79b0450ad5
thomas-randell <119977694+Thomas-Randell@users.noreply.github.com>
2023-06-25 13:01:28 +0100

    Size square divs to fit grid By using border-box and console logging width of the containing div, the 16 x 16 grid of divs now fits perfectly.

--- eas.js
+++ eas.js
@@ -1,16 +1,19 @@
 let grid = [];
 let container = document.querySelector(".container");
 let containerWidth = container.offsetWidth;
-let gridWidth = 8;
+let gridWidth = 16;
 let gridSize = gridWidth ** 2;
 
 
 for (let i = 0; i < gridSize; i++) {
     div = document.createElement('div');
-    div.style.width = (containerWidth / (gridWidth)) - 4 + "px";
+    div.style.width = ((containerWidth - 2) / (gridWidth)) + "px";
     div.style.height = div.style.width;
     div.classList.add("square");
     grid.push(div);
 }
 
-    grid.forEach(square => container.appendChild(square));
+grid.forEach(square => container.appendChild(square));
+
+console.log(containerWidth);
+console.log(div.style.width)
