5e2dd027c812cb14bf410914d9880b59c4f9f8bc
thomas-randell <119977694+Thomas-Randell@users.noreply.github.com>
2023-06-25 13:46:05 +0100

    Add button to reset and prompt new grid size Functions to setup grid by resetting and prompting for new amount of square per side. Project criteria complete.

--- eas.js
+++ eas.js
@@ -1,20 +1,33 @@
-let grid = [];
-let container = document.querySelector(".container");
-let containerWidth = container.offsetWidth;
-let gridWidth = 16;
-let gridSize = gridWidth ** 2;
+const container = document.querySelector(".container");
+const containerWidth = container.offsetWidth;
 
+function gridSetup(gridWidth) {
+    let grid = [];
+    let gridSize = gridWidth ** 2;
+    for (let i = 0; i < gridSize; i++) {
+        div = document.createElement('div');
+        div.style.width = ((containerWidth - 2) / (gridWidth)) + "px";
+        div.style.height = div.style.width;
+        div.classList.add("square");
+        grid.push(div);
+    }
+    grid.forEach(square => container.appendChild(square));
+    grid.forEach(square => square.addEventListener("mouseover", function () {
+        square.style.background = "black";
+    }))
 
-for (let i = 0; i < gridSize; i++) {
-    div = document.createElement('div');
-    div.style.width = ((containerWidth - 2) / (gridWidth)) + "px";
-    div.style.height = div.style.width;
-    div.classList.add("square");
-    grid.push(div);
 }
 
-grid.forEach(square => container.appendChild(square));
+function resetGrid() {
+    document.querySelector(".container").innerHTML = "";
+}
 
-grid.forEach(square => square.addEventListener("mouseover", function () {
-    square.style.background = "black";
-}))
+gridSetup(16)
+
+btn = document.querySelector("button")
+btn.addEventListener("click", function () {
+    let userChoice = prompt("How many squares per side?")
+    resetGrid()
+    gridSetup(userChoice)
+})
+
